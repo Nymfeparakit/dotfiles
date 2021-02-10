90ec36a32c063ebe8fcc8021a9882c642537f997
Nymfeparakit <maxlex047@gmail.com>
2021-02-10 23:00:09 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,15 @@
 # installing packages
-sudo apt install i3;
-sudo apt install zsh;
-sudo apt install curl;
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)";
+sudo apt install i3
+sudo apt install i3blocks
+sudo apt install zsh
+sudo apt install curl
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # linking config files
-rm ~/.config/i3/config;
-mkdir -p ~/.config/i3;
-ln -s ~/dotfiles/i3/config ~/.config/i3/config;
+rm ~/.config/i3/config
+mkdir -p ~/.config/i3
+ln -s ~/dotfiles/i3/config ~/.config/i3/config
+
 ln -s ~/dotfiles/.vimrc ~/.vimrc
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall

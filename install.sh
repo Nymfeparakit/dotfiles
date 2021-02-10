91fc0b995ce9c0786e54f8c6b6ce455ecfad204f
Nymfeparakit <maxlex047@gmail.com>
2021-02-10 21:53:43 +0300

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+# installing packages
+sudo apt install i3;
+sudo apt install zsh;
+sudo apt install curl;
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)";
+
+# linking config files
+rm ~/.config/i3/config;
+ln -s ~/dotfiles/i3/config ~/.config/i3/config;
+ln -s ~/dotfiles/.vimrc ~/.vimrc

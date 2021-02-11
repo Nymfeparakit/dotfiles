4d11b1c60f7f77c60a2fdd1142740404bb7f9b36
Nymfeparakit <maxlex047@gmail.com>
2021-02-11 22:32:41 +0300

    Dowload fonts

--- install.sh
+++ install.sh
@@ -5,6 +5,13 @@ sudo apt install zsh
 sudo apt install curl
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+# download noto fonts
+curl https://noto-website-2.storage.googleapis.com/pkgs/NotoColorEmoji-unhinted.zip --output noto_emoji.zip 
+cd ~/Downloads
+mkdir ~/.fonts
+cp *otf *otc ~/.fonts
+fc-cache -f -v
+
 # linking config files
 rm ~/.config/i3/config
 mkdir -p ~/.config/i3
